e785a3f143dfb5f7d24a79a662fdcea35edb2e6a
bakkenal <>
2021-10-01 14:15:52 -0500

    Removed linter, halfway-implemented bracket organizer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {RoundProps} from './utilities/bracket-organizer.ts';
+
+const list = ["a ", "b ", "c", "d"];
+
+const formBracket = () => {
+	return <Bracket rounds={rounds} />;
+};
 
 function App() {
   return (
@@ -7,7 +14,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>
         </p>
         <a
           className="App-link"
@@ -15,11 +22,42 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {formBracket}
         </a>
       </header>
     </div>
   );
 }
+
+// given an array arr and length n (optional), shuffle array
+const shuffle = (arr, n) => {
+	if (n === undefined) {
+		n = arr.length;
+	}
+	else if (n <= 0) {
+		n = arr.length;
+		console.warn('Requested samples is not greater than 0. Using full array.');
+	}
+	else if (n > arr.length) {
+		n = arr.length;
+		console.warn('Requested more samples than there are available. Using full array.');
+	}
+	let nInd = n;
+
+	let currIndex = arr.length, tempValue, randIndex;
+	// While there remain elements to shuffle...
+	while (0 !== currIndex) {
+		// Pick a remaining element...
+		randIndex = Math.floor(Math.random() * currIndex);
+		currIndex -= 1;
+
+		// And swap it with the current element.
+		tempValue = arr[currIndex];
+		arr[currIndex] = arr[randIndex];
+		arr[randIndex] = tempValue;
+	}
+	return arr.slice(0, nInd);
+};
+
 
 export default App;
