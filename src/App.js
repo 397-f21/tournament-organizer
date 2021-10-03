566f08456f34bbd4b0993492bb3a87798f6c6c04
Jonnyi789 <johnny080104@gmail.com>
2021-10-03 16:16:19 -0500

    add submitted name to list

--- src/App.js
+++ src/App.js
@@ -11,19 +11,23 @@ import { Bracket } from 'react-brackets';
 const App = () => {
   const [buttonPressed, setButtonPressed] = useState(false);
 
-  const [names, setNames] = useState([]);
+  const [names, setNames] = useState(['bob']); //names is a list
 
   const clickHandler = () => {
     setButtonPressed(true);
   }
 
+  const addName = (name) => {  //name is a str
+    setNames(names => [...names, name]);
+    //console.log(name);
+  }
   return (
     <div className="App">
       <header className="App-header">
       <div className="names-list">
       <ol>{ 
-          names.map(name =>
-            <li>{name}</li>
+          names.map((name, key)=>
+            <li key={key}>{name}</li>
             ) 
           
         }</ol>
@@ -32,7 +36,7 @@ const App = () => {
         <div>
           <form>
             {!(buttonPressed) && <input type='text'></input>}
-            {!(buttonPressed) && <input type='submit'></input>}
+            {!(buttonPressed) && <input type='submit' onClick={e => addName(e.target.value)}></input>}
           </form>
         </div>
         <div>
