b29f5482a0eaf0c22bd714a24c84660e73ce9806
Brian Tang <btang@blt-macbook.lan>
2021-10-03 16:46:10 -0500

    user input - working

--- src/App.js
+++ src/App.js
@@ -11,32 +11,36 @@ import { Bracket } from 'react-brackets';
 const App = () => {
   const [buttonPressed, setButtonPressed] = useState(false);
 
-  const [names, setNames] = useState(['bob']); //names is a list
+  const [names, setNames] = useState([]); //names is a list
+
+  const [input, updateInput] = useState('');
 
   const clickHandler = () => {
     setButtonPressed(true);
   }
 
-  const addName = (name) => {  //name is a str
-    setNames(names => [...names, name]);
-    //console.log(name);
+  const changeHandler = txt => {
+    updateInput(txt);
   }
+
+  const addName = () => {  //name is a str
+    setNames(names => [...names, input]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-      <div className="names-list">
-      <ol>{ 
-          names.map((name, key)=>
-            <li key={key}>{name}</li>
-            ) 
-          
-        }</ol>
+        <div className="names-list">
+          <ol>
+            {names.map((name, key)=>
+              <li key={key}>{name}</li>
+            )}
+          </ol>
         </div>
-
         <div>
           <form>
-            {!(buttonPressed) && <input type='text'></input>}
-            {!(buttonPressed) && <input type='submit' onClick={e => addName(e.target.value)}></input>}
+            {!(buttonPressed) && <input type='text' onChange={e => changeHandler(e.target.value)}></input>}
+            {!(buttonPressed) && <input type='submit' onClick={addName}></input>}
           </form>
         </div>
         <div>
