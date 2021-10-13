6ed12287bd9a2e8278242f40f2155ffd5ca45ac2
Alex <alexgold1100@gmail.com>
2021-10-13 14:41:33 -0500

    Rounding up to the next power of 2

--- src/App.js
+++ src/App.js
@@ -12,6 +12,24 @@ const App = () => {
   const [names, setNames] = useState([]); //names is a list
 
   const clickHandler = () => {
+
+    // make this a function later (rounds up to the next power of 2);
+    let numGiven = names.length;
+    let nextPowOf2 = Math.pow(2, Math.round(Math.log(numGiven) / Math.log(2)));
+
+    let n = numGiven - 1;
+    while ((n & n - 1) !== 0) {
+      n = n & n - 1;
+    }
+    n = n << 1;
+
+
+    console.log("numGiven: " + numGiven);
+    console.log("nextPowerOf2: " + n);
+    for (let i = 0; i < nextPowOf2 - numGiven; i++) {
+      names.push("BYE");
+    }
+    setNames(names);
     setButtonPressed(true);
   }
 
