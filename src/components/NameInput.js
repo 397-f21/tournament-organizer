6b619bbdd3d4dc5fef05d11082571da0488f5467
Alex <alexgold1100@gmail.com>
2021-10-15 15:36:59 -0500

    Name input file updates

--- src/components/NameInput.js
+++ src/components/NameInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const addName = (input, names, setNames) => {  //name is a str
     if (!input.trim()) {
